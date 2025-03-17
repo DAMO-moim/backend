258773df54dcb769f88a647b0b5416397aec688d
sw8865@naver.com <kie9400@syuin.ac.kr>
2025-03-18 00:51:39 +0900

    swagger test

--- src/main/java/com/springboot/member/controller/MemberController.java
+++ src/main/java/com/springboot/member/controller/MemberController.java
@@ -5,6 +5,9 @@ import com.springboot.member.entity.Member;
 import com.springboot.member.mapper.MemberMapper;
 import com.springboot.member.service.MemberService;
 import com.springboot.utils.UriCreator;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.net.URI;
 
+@ApiOperation(value = "회원 정보 API", tags = {"Member Controller"})
 @RestController
 @RequestMapping("/test/members")
 @Validated
@@ -24,6 +28,13 @@ public class MemberController {
         this.memberService = memberService;
         this.mapper = mapper;
     }
+
+    @ApiOperation(value = "회원 정보 등록", notes = "회원 정보를 등록합니다.")
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "회원 등록 완료"),
+            @ApiResponse(code = 404, message = "Member not found")
+    })
     @PostMapping
     public ResponseEntity postMember(@RequestBody @Valid MemberDto.Post memberPostDto) {
         // Mapper를 통해 받은 Dto 데이터 Member로 변환
