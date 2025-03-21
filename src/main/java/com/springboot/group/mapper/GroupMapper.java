7c312db6441587c7e8cd3b0fc3b75ca02b740b47
택현 <tjsk2222@gmail.com>
2025-03-21 20:50:50 +0900

    GroupMapper 일부 커스텀 (계속해서 변경할 가능성 있음)

--- src/main/java/com/springboot/group/mapper/GroupMapper.java
+++ src/main/java/com/springboot/group/mapper/GroupMapper.java
@@ -2,14 +2,51 @@ package com.springboot.group.mapper;
 
 import com.springboot.group.dto.GroupDto;
 import com.springboot.group.entity.Group;
+import com.springboot.member.dto.MemberDto;
+import com.springboot.tag.dto.GroupTagResponseDto;
+import com.springboot.tag.dto.TagResponseDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface GroupMapper {
     Group groupPostToGroup(GroupDto.Post groupPost);
     Group groupPatchToGroup(GroupDto.Patch groupPatch);
-    GroupDto.Response groupToGroupResponse(Group group);
-    List<GroupDto.Response> groupsToGroupResponses(List<Group> groups);
+    default GroupDto.Response groupToGroupResponse(Group group) {
+        GroupDto.Response.ResponseBuilder builder = GroupDto.Response.builder()
+//                .categoryId(group.getSubCategory().getCategory().getCategoryId())
+                .groupId(group.getGroupId())
+                .name(group.getGroupName())
+                .introduction(group.getIntroduction())
+                .maxMemberCount(group.getMaxMemberCount())
+                .memberCount(group.getGroupMembers().size())
+                .gender(group.getGender())
+                .minBirth(group.getMinBirth())
+                .maxBirth(group.getMaxBirth())
+//                .subCategoryName(group.getSubCategory().getSubCategoryName())
+                // ✅ 멤버 리스트 변환
+                .members(group.getGroupMembers().stream()
+                        .map(groupMember -> MemberDto.MemberOfGroupResponse.builder()
+                                .memberId(groupMember.getMember().getMemberId())
+                                .name(groupMember.getMember().getName())
+                                .build())
+                        .collect(Collectors.toList()))
+
+                // ✅ 태그 리스트 변환
+                .tags(group.getGroupTags().stream()
+                        .map(groupTag -> GroupTagResponseDto.builder()
+                                .tagId(groupTag.getTag().getTagId())
+                                .tagName(groupTag.getTag().getTagName())
+                                .build())
+                        .collect(Collectors.toList()));
+
+                return builder.build();
+    }
+    default List<GroupDto.Response> groupsToGroupResponses(List<Group> groups) {
+        return groups.stream()
+                .map(this::groupToGroupResponse) // ✅ 단일 조회용 매핑 재사용
+                .collect(Collectors.toList());
+    }
 }
