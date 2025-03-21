1416e5fc9df2f5e3ff1adb7346bfd1a58461c34f
택현 <tjsk2222@gmail.com>
2025-03-21 20:45:14 +0900

    GroupDto 일부 수정

--- src/main/java/com/springboot/group/dto/GroupDto.java
+++ src/main/java/com/springboot/group/dto/GroupDto.java
@@ -1,15 +1,17 @@
 package com.springboot.group.dto;
 
+import com.springboot.group.entity.Group;
+import com.springboot.member.dto.MemberDto;
 import com.springboot.member.entity.Member;
 import com.springboot.schedule.entity.Schedule;
+import com.springboot.tag.dto.GroupTagResponseDto;
+import com.springboot.tag.dto.TagResponseDto;
 import com.springboot.tag.entity.Tag;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 import java.util.List;
 
 public class GroupDto {
@@ -23,29 +25,27 @@ public class GroupDto {
         @Schema(description = "모임소개", example = "아마추어 5단 이상의 노인네 모임입니다.")
         private String introduction;
 
-        @NotBlank(message = "최대 인원수는 필수사항입니다.")
+        @Min(value = 2, message = "모임 인원은 최소 2명 이상이어야 합니다.")
+        @Max(value = 100, message = "모임 인원은 최대 100명까지만 가능합니다.")
         @Schema(description = "모임 최대 인원 수", example = "20")
         private int maxMemberCount;
 
-        @NotBlank
         @Schema(description = "성별", example = "무관")
-        private Member.Gender gender;
+        private Group.GroupGender gender;
 
-        @NotBlank
+        @NotNull
         @Schema(description = "최소년생", example = "1720년생")
         private String minBirth;
 
-        @NotBlank
+        @NotNull
         @Schema(description = "최대년생", example = "2500년생")
         private String maxBirth;
 
-        @NotBlank
         @Parameter(description = "서브카테고리 ID", example = "1")
         private Long subCategoryId;
 
-        @NotBlank
         @Schema(description = "태그들 ID 목록", example = "[{\"tagId\": 1}, {\"tagId\": 2}]")
-        private List<Tag> tags;
+        private List<Tag> groupTags;
     }
 
     @Getter
@@ -62,9 +62,13 @@ public class GroupDto {
 
     @AllArgsConstructor
     @Getter
+    @Builder
+    @Setter
+    @NoArgsConstructor
     public static class Response {
 
         @Parameter(description = "카테고리 ID", example = "1")
+        @Null
         private Long categoryId;
 
         @Parameter(description = "그룹 ID", example = "1")
@@ -86,7 +90,7 @@ public class GroupDto {
         private int maxMemberCount;
 
         @Schema(description = "모임 최대 인원 수", example = "20")
-        private Member.Gender gender;
+        private Group.GroupGender gender;
 
         @Schema(description = "최소년생", example = "1750년")
         private String minBirth;
@@ -94,16 +98,16 @@ public class GroupDto {
         @Schema(description = "최대년생", example = "2200년")
         private String maxBirth;
 
-        @Schema(description = "모임명", example = "바둑 아마추어 5단이상 노장모임")
-        private Long subCategoryId;
+        @Schema(description = "서브 카테고리 이름", example = "축구, 힙합, 어쩌고")
+        private String subCategoryName;
 
         @Schema(description = "모임 멤버 목록",
                 example = "[{\"memberId\": 1, \"name\": \"홍길동\", \"Image\": \"https://example.com/profiles/alice.jpg\"}, " +
                         "{\"memberId\": 2, \"name\": \"김철수\", \"Image\": \"https://example.com/profiles/bob.jpg\"}]")
-        private List<Member> members;
+        private List<MemberDto.MemberOfGroupResponse> members;
 
         @Schema(description = "태그들 ID 목록", example = "[{\"tagId\": 1}, {\"tagId\": 2}]")
-        private List<Tag> tags;
+        private List<GroupTagResponseDto> tags;
 
         @Schema(description = "모임 일정 목록",
                 example = "[{\"groupScheduleId\": 1, \"startschedule\": \"2025.03.16 10:00\", \"endschedule\": \"2025.03.16 10:00\", " +
