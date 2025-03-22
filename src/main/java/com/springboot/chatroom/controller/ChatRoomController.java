dcbd838dc9739411805184e4a36ee66f891b8021
hogeun <sw8865@naver.com>
2025-03-22 19:01:34 +0900

    chatRoom Repository 구현

--- src/main/java/com/springboot/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/springboot/chatroom/controller/ChatRoomController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/chatrooms") // ✅ 모든 엔드포인트의 기본 URL
 public class ChatRoomController {
+    //하나의 카테고리 채팅방 조회
     @GetMapping({"category-id"})
     public ResponseEntity getChatRoom(@PathVariable("category-id") long categoryId,
                                       @Schema(hidden = true) @AuthenticationPrincipal Member member){
@@ -20,6 +21,7 @@ public class ChatRoomController {
         return null;
     }
 
+    //회원의 카테고리별 채팅방 목록 조회
     @GetMapping
     public ResponseEntity getChatRooms(@Schema(hidden = true) @AuthenticationPrincipal Member member){
 
--- src/main/java/com/springboot/chatroom/service/ChatRoomService.java
+++ src/main/java/com/springboot/chatroom/service/ChatRoomService.java
@@ -1,4 +1,11 @@
 package com.springboot.chatroom.service;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class ChatRoomService {
+
 }
