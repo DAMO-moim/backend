03cfd934908a9675e556596581c04da435b3e42e
택현 <tjsk2222@gmail.com>
2025-03-24 02:36:14 +0900

    모임생성 시 tagName으로 받기 위해 Dto 생성

---
+++ src/main/java/com/springboot/tag/dto/TagNameDto.java
@@ -0,0 +1,14 @@
+package com.springboot.tag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagNameDto {
+    private String tagName;
+}
