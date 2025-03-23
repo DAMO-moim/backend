d88d4596bf8e2b56e085e19fa330895bde4fe8d8
hogeun <sw8865@naver.com>
2025-03-23 22:05:19 +0900

    mypage dto 구현

--- src/main/java/com/springboot/member/dto/MyPageDto.java
+++ src/main/java/com/springboot/member/dto/MyPageDto.java
@@ -1,4 +1,20 @@
 package com.springboot.member.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
 public class MyPageDto {
+    @Getter
+    @AllArgsConstructor
+    public static class BoardsResponse{
+        private Long boardId;
+        private String title;
+        private String contentPreview;
+        private String category;
+        private int commentCount;
+        private LocalDate createdAt;
+        private String groupName;
+    }
 }
