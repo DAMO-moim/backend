7ac48abd6309898ea36027a30bd7c431b93c9de5
택현 <tjsk2222@gmail.com>
2025-03-25 02:24:44 +0900

    ScheduleService 일부 구현

--- src/main/java/com/springboot/schedule/service/ScheduleService.java
+++ src/main/java/com/springboot/schedule/service/ScheduleService.java
@@ -1,27 +1,39 @@
 package com.springboot.schedule.service;
 
+import com.springboot.group.service.GroupService;
+import com.springboot.schedule.dto.ScheduleDto;
 import com.springboot.schedule.entity.Schedule;
+import com.springboot.schedule.repository.ScheduleRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScheduleService {
-    public Schedule createSchedule() {
+    private final ScheduleRepository scheduleRepository;
+    private final GroupService groupService;
+
+    public ScheduleService(ScheduleRepository scheduleRepository, GroupService groupService) {
+        this.scheduleRepository = scheduleRepository;
+        this.groupService = groupService;
+    }
+
+    public Schedule createSchedule(Schedule schedule, long groupId, long memberId) {
         return null;
     }
 
-    public Schedule updateSchedule() {
+    public Schedule updateSchedule(Schedule schedule, long groupId, long memberId) {
         return null;
     }
 
-    public Schedule findSchedule() {
+    public Schedule findSchedule(long memberId, long groupId, long scheduleId) {
         return null;
     }
 
-    public Schedule findSchedules() {
+    public Page<Schedule> findSchedules(int page, int size, long memberId) {
         return null;
     }
 
-    public Schedule deleteSchedule() {
+    public Schedule deleteSchedule(long memberId, long groupId, long scheduleId) {
         return null;
     }
 }
