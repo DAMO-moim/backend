8b97740265130715414e60319a1227919fd2052d
택현 <tjsk2222@gmail.com>
2025-03-24 20:20:41 +0900

    일정 CRUD Controller 구현

---
+++ src/main/java/com/springboot/schedule/controller/ScheduleController.java
@@ -0,0 +1,53 @@
+package com.springboot.schedule.controller;
+
+import com.springboot.member.entity.Member;
+import com.springboot.schedule.dto.ScheduleDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping("/groups")
+@Validated
+public class ScheduleController {
+    @PostMapping("/{group-id}/schedules")
+    public ResponseEntity postSchedule(@RequestBody ScheduleDto.Post schedulePostDto,
+                                       @AuthenticationPrincipal Member authenticatedmember,
+                                       @PathVariable("group-id") @Positive long groupId) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{group-id}/schedules/{schedule-id}")
+    public ResponseEntity patchSchedule(@RequestBody ScheduleDto.Patch schedulePatchDto,
+                                       @AuthenticationPrincipal Member authenticatedmember,
+                                       @PathVariable("group-id") @Positive long groupId,
+                                       @PathVariable("schedule-id") @Positive long scheduleId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{group-id}/schedules/{schedule-id}")
+    public ResponseEntity getSchedule(@RequestBody ScheduleDto.Response scheduleResponseDto,
+                                      @AuthenticationPrincipal Member authenticatedmember,
+                                      @PathVariable("group-id") @Positive long groupId,
+                                      @PathVariable("schedule-id") @Positive long scheduleId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{group-id}/schedules")
+    public ResponseEntity getSchedules(@RequestParam @Positive int page,
+                                       @RequestParam @Positive int size,
+                                       @AuthenticationPrincipal Member authenticatedmember) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{group-id}/schedules/{schedule-id}")
+    public ResponseEntity deleteSchedule(@AuthenticationPrincipal Member authenticatedmember,
+                                          @PathVariable("group-id") @Positive long groupId,
+                                          @PathVariable("schedule-id") @Positive long scheduleId) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
