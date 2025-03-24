c6d706b692737fb774840c9b02821e756124b244
택현 <tjsk2222@gmail.com>
2025-03-24 20:24:05 +0900

    ScheduleService 틀 구현

---
+++ src/main/java/com/springboot/schedule/service/ScheduleService.java
@@ -0,0 +1,27 @@
+package com.springboot.schedule.service;
+
+import com.springboot.schedule.entity.Schedule;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleService {
+    public Schedule createSchedule() {
+        return null;
+    }
+
+    public Schedule updateSchedule() {
+        return null;
+    }
+
+    public Schedule findSchedule() {
+        return null;
+    }
+
+    public Schedule findSchedules() {
+        return null;
+    }
+
+    public Schedule deleteSchedule() {
+        return null;
+    }
+}
