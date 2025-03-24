915bd60e2ef420808e1090aee7ad5ac76da6a63e
unknown <sw8865@naver.com>
2025-03-24 18:54:59 +0900

    adminmapper 구현

--- src/main/java/com/springboot/member/mapper/AdminMapper.java
+++ src/main/java/com/springboot/member/mapper/AdminMapper.java
@@ -1,4 +1,11 @@
 package com.springboot.member.mapper;
 
+import com.springboot.member.dto.AdminDto;
+import com.springboot.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AdminMapper {
+    AdminDto.MemberResponse responseDtoToMember(Member member);
 }
