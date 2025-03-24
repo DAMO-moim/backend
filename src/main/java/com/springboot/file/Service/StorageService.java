8809b59117b1b289649985fcd272bb93d0c327d5
unknown <sw8865@naver.com>
2025-03-24 12:04:12 +0900

    이미지 파일 관련 클래스(로컬) 임시 구현

--- src/main/java/com/springboot/file/Service/StorageService.java
+++ src/main/java/com/springboot/file/Service/StorageService.java
@@ -3,5 +3,5 @@ package com.springboot.file.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
-    void store(MultipartFile file);
+    String store(MultipartFile file, String fileName);
 }
