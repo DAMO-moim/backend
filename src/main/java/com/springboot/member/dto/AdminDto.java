4d44c80f9b30e6cb5f3aae034bc52eda9ab681cd
unknown <sw8865@naver.com>
2025-03-24 18:54:40 +0900

    admindto 구현

--- src/main/java/com/springboot/member/dto/AdminDto.java
+++ src/main/java/com/springboot/member/dto/AdminDto.java
@@ -1,4 +1,26 @@
 package com.springboot.member.dto;
 
+import com.springboot.member.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 public class AdminDto {
+    @AllArgsConstructor
+    @Getter
+    public static class MemberResponse{
+        private long memberId;
+
+        @Schema(description = "사용자 이메일", example = "example@gmail.com")
+        private String email;
+
+        @Schema(description = "사용자 프로필 이미지", example = "/profile")
+        private String image;
+
+        @Schema(description = "사용자 이름", example = "홍성민")
+        private String name;
+
+        @Schema(description = "사용자 성별", example = "Girl")
+        private Member.Gender gender;
+    }
 }
