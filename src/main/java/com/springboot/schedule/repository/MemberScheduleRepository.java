e8da7b558b72d83d01d57c6dcaab4945a8e6fcd0
unknown <sw8865@naver.com>
2025-03-25 19:46:55 +0900

    해당 일정에 참여하고 있는지 확인하기 위한 쿼리사용

--- src/main/java/com/springboot/schedule/repository/MemberScheduleRepository.java
+++ src/main/java/com/springboot/schedule/repository/MemberScheduleRepository.java
@@ -5,6 +5,9 @@ import com.springboot.member.entity.MemberSchedule;
 import com.springboot.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberScheduleRepository extends JpaRepository<MemberSchedule, Long> {
     boolean existsByScheduleAndMember(Schedule schedule, Member member);
+    Optional<MemberSchedule> findByMemberAndSchedule(Member member, Schedule schedule);
 }
