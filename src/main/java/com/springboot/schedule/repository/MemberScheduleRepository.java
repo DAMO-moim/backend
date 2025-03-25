ef931a7a34d089b078c6dc42d777f4b504a4c880
unknown <sw8865@naver.com>
2025-03-25 19:36:03 +0900

    이미 참여하고 있는 회원 검증을 위한 쿼리사용

--- src/main/java/com/springboot/schedule/repository/MemberScheduleRepository.java
+++ src/main/java/com/springboot/schedule/repository/MemberScheduleRepository.java
@@ -1,4 +1,10 @@
 package com.springboot.schedule.repository;
 
-public interface MemberScheduleRepository {
+import com.springboot.member.entity.Member;
+import com.springboot.member.entity.MemberSchedule;
+import com.springboot.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberScheduleRepository extends JpaRepository<MemberSchedule, Long> {
+    boolean existsByScheduleAndMember(Schedule schedule, Member member);
 }
