941a3db9a0c987b5129b50c200f9cbb38d796782
unknown <sw8865@naver.com>
2025-03-26 16:26:58 +0900

    카테고리별 일정 조희 비즈니스 로직 구현

--- src/main/java/com/springboot/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/springboot/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,11 @@
 package com.springboot.schedule.repository;
 
 import com.springboot.schedule.entity.Schedule;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
