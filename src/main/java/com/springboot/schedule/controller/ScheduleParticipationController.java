52a79c22cd03fe0c9e7895eae8e7a2b84c12f3ba
택현 <tjsk2222@gmail.com>
2025-03-26 11:02:35 +0900

    모임 일정 참여 회원 리스트 Controller 구현

--- src/main/java/com/springboot/schedule/controller/ScheduleParticipationController.java
+++ src/main/java/com/springboot/schedule/controller/ScheduleParticipationController.java
@@ -1,16 +1,27 @@
 package com.springboot.schedule.controller;
 
+import com.springboot.dto.SingleResponseDto;
 import com.springboot.member.entity.Member;
+import com.springboot.schedule.dto.ParticipantInfoDto;
+import com.springboot.schedule.service.ScheduleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/schedules")
 @Validated
 public class ScheduleParticipationController {
+    private final ScheduleService scheduleService;
+
+    public ScheduleParticipationController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
     @PostMapping("/{schedule-id}/participation")
     public ResponseEntity postParticipationSchedule(@PathVariable("schedule-id") long scheduleId,
                                                     @AuthenticationPrincipal Member authenticatedmember) {
@@ -19,8 +30,10 @@ public class ScheduleParticipationController {
 
     @GetMapping("/{schedule-id}/participation")
     public ResponseEntity getParticipationSchedule(@PathVariable("schedule-id") long scheduleId,
-                                                    @AuthenticationPrincipal Member authenticatedmember) {
-        return new ResponseEntity<>(HttpStatus.OK);
+                                                   @AuthenticationPrincipal Member authenticatedMember,
+                                                   @RequestParam(value= "keyword", required = false) String keyword) {
+        List<ParticipantInfoDto> response = scheduleService.findScheduleParticipants(scheduleId, authenticatedMember.getMemberId(), keyword);
+        return ResponseEntity.ok(new SingleResponseDto<>(response));
     }
 
     @DeleteMapping("/{schedule-id}/participation")
