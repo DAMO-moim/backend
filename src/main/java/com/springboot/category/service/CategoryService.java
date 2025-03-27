46ab7eb8221294422b18954f222307ac062467d6
unknown <sw8865@naver.com>
2025-03-27 12:14:58 +0900

    카테고리별 서브 카테고리 조회 비즈니스로직 구현

--- src/main/java/com/springboot/category/service/CategoryService.java
+++ src/main/java/com/springboot/category/service/CategoryService.java
@@ -1,15 +1,18 @@
 package com.springboot.category.service;
 
 import com.springboot.category.entity.Category;
+import com.springboot.category.entity.SubCategory;
 import com.springboot.category.repository.CategoryRepository;
 import com.springboot.exception.BusinessLogicException;
 import com.springboot.exception.ExceptionCode;
 import com.springboot.member.entity.Member;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
@@ -34,5 +37,12 @@ public class CategoryService {
         Optional<Category> optionalCategory = categoryRepository.findByCategoryName(categoryName);
         optionalCategory.orElseThrow(() ->
                 new BusinessLogicException(ExceptionCode.CATEGORY_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubCategory> findSubCategoriesByCategoryId(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.CATEGORY_NOT_FOUND));
+        return category.getSubCategories();
     }
 }
