572a0632173d0dddbfad7798b534b40c25029657
unknown <sw8865@naver.com>
2025-03-31 11:12:54 +0900

    s3 적용 test

--- src/main/java/com/springboot/config/StorageConfiguration.java
+++ src/main/java/com/springboot/config/StorageConfiguration.java
@@ -16,10 +16,10 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class StorageConfiguration {
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
