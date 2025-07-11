8aab450077523cf079b15dc87908eff0299c8955
택현 <tjsk2222@gmail.com>
2025-07-11 16:00:46 +0900

    인스턴스 주소 변경으로 인해 접근 허용 주소 변경 및 redis host 주소 변경

--- src/main/java/com/springboot/config/SecurityConfiguration.java
+++ src/main/java/com/springboot/config/SecurityConfiguration.java
@@ -131,7 +131,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // 모든 출처에서 통신 허용
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.39.190.50"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.37.55.53"));
 //        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
         // 전체 요청 -> 특정 요청 Arrays.asList("http://localhost:3000");
         // 지정한 HTTP Method 에 대한 통신 허용
